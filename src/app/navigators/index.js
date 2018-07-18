7dad38eb0908ef0a340daf6768ae942737d2dbec
harshasanne <sriharsha.sanne@sv.cmu.edu>
2018-07-17 18:42:12 -0700

    Removed commented code

--- src/app/navigators/index.js
+++ src/app/navigators/index.js
@@ -1,17 +1,14 @@
 import { createStackNavigator } from 'react-navigation';
 import {reduxifyNavigator, createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers';
 import { connect } from 'react-redux';
+
 import * as ScreenNames from './screen_names';
 import HomeScreen from '../containers/Home';
 import SignupScreen from '../containers/Signup';
 import SignupVerifyScreen from '../containers/Signup/verify';
 import MapScreen from '../containers/Map';
-<<<<<<< HEAD
 import ProfileScreen from '../containers/Profile';
-=======
 import ContactsScreen from '../containers/Contacts';
-
->>>>>>> Added contacts screen and linear gradient to home page
 
 const navMiddleware = createReactNavigationReduxMiddleware(
   'root',
@@ -24,18 +21,12 @@ const RootNavigator = createStackNavigator(
     [ScreenNames.SIGNUP]: {screen:SignupScreen},
     [ScreenNames.SIGNUP_VERIFY]: {screen:SignupVerifyScreen},
     [ScreenNames.MAP]: {screen:MapScreen},
-<<<<<<< HEAD
-    [ScreenNames.USER_PROFILE]: {screen:ProfileScreen}
-=======
+    [ScreenNames.USER_PROFILE]: {screen:ProfileScreen},
     [ScreenNames.CONTACTS]: {screen:ContactsScreen}
-<<<<<<< HEAD
-
->>>>>>> Added contacts screen and linear gradient to home page
-=======
->>>>>>> Changes based on PR review
   },
   {
     initialRouteName: ScreenNames.HOME
+
   }
 );
 
