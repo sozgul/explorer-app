502542ade57cca7445a028aa213e9e89924eb6d9
Meenu Thayil <meenu.thayil@sv.cmu.edu>
2018-07-25 15:59:26 -0700

    Updating the main flow to signup screen

--- src/app/navigators/index.js
+++ src/app/navigators/index.js
@@ -18,7 +18,7 @@ const RootNavigator = createStackNavigator(
     [ScreenNames.MAP_FLOW]: MapNavigator
   },
   {
-    initialRouteName: ScreenNames.MAIN_FLOW,
+    initialRouteName: ScreenNames.SIGNUP_FLOW,
     headerMode: 'none'
   }
 );
