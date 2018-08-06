c160eed43aad83280d3164b373c5a81c0e584b9c
harshasanne <sriharsha.sanne@sv.cmu.edu>
2018-08-05 19:02:02 -0700

    Added Message List component

--- src/app/components/Message/index.js
+++ src/app/components/Message/index.js
@@ -1,47 +1,37 @@
 import React, {Component} from 'react';
-import { Text, View,ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
-import { TRANSPARENT} from '../../common/colors';
-import commonStyles from '../../common/styles';
-import { LinearGradient } from 'expo';
+import { MEDIUM_GREY} from '../../common/colors';
+import MessageListItem from './item';
 
-class MessageComponent extends Component {
+
+class MessageList extends Component {
+  _getColorForSender(senderID) {
+    const senderColorItem = this.props.colors.find(item => item.senderID === senderID);
+    return senderColorItem ? senderColorItem.color : MEDIUM_GREY;
+  }
+
   render() {
-
-
     return (
-      <View style={styles.contentContainer}>
-        <ScrollView>
-
-          <Text style= {[commonStyles.text,styles.textRight]}>Meenu</Text>
-          <View style={styles.bubbles}>
-            <LinearGradient colors={[TRANSPARENT,'rgba(255,255,255,0.5)','rgba(255,255,255,0.6)']}
-              start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}} style = {styles.bubbles}
-            >
-              <Text style= {[commonStyles.text,styles.text]}>Hi Serhan!!</Text>
-            </LinearGradient>
-
-          </View>
-          <Text style= {[commonStyles.text,styles.textRight]}>Serhan</Text>
-
-          <LinearGradient colors={[TRANSPARENT,'rgba(255,255,255,0.5)','rgba(255,255,255,0.6)']}
-            start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}} style = {styles.bubbles}
-          >
-            <Text style= {[commonStyles.text,styles.text]}>Hello Meenu How are you</Text>
-          </LinearGradient>
-        </ScrollView>
-      </View>
+      <ScrollView style={styles.contentContainer}>
+        {this.props.messages.map(message => (
+          <MessageListItem
+            key={message.id}
+            message={message}
+            senderColor={this._getColorForSender(message.senderID)}
+          />)
+        )}
+      </ScrollView>
     );
   }
 }
 
-MessageComponent.propTypes = {
-  text: PropTypes.string,
-  onPress: PropTypes.func,
-  style: PropTypes.any,
-  textStyle: PropTypes.any,
-  disabled: PropTypes.bool
+MessageList.propTypes = {
+  messages: PropTypes.array.isRequired,
+  colors: PropTypes.array.isRequired
 };
 
-export default MessageComponent;
+
+
+export default MessageList;
---
+++ src/app/components/Message/item.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+import commonStyles from '../../common/styles';
+import { LinearGradient } from 'expo';
+
+
+class MessageListItem extends Component {
+  render() {
+    const {message,senderColor} = this.props;
+    const {content, sender} = message;
+    const dotStyles = [styles.dot];
+    if (senderColor) {
+      dotStyles.push({backgroundColor:senderColor});
+    }
+    return (
+      <View>
+        <Text style= {[commonStyles.text,styles.textRight]}>{sender}</Text>
+        <LinearGradient colors={['rgba(255,255,255,0.7)','rgba(255,255,255,0.9)']}
+          start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}} style = {styles.buttonContainer}
+        >
+          <View style= {dotStyles}></View>
+          <Text style= {[commonStyles.text,styles.text]}>{content}</Text>
+        </LinearGradient>
+      </View>
+    );
+  }
+}
+
+MessageListItem.propTypes = {
+  message: PropTypes.object.isRequired,
+  senderColor: PropTypes.string.isRequired
+};
+export default MessageListItem;
